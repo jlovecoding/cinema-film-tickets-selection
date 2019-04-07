a7b575b2cda15ae99a6297323a9406ce54a45dbd
jlovecoding <jlovecoding@gmail.com>
2019-04-07 22:30:41 +0200

    add spring cloud config client and config it to take config from cinema-config-repository

--- src/main/java/com/cinema/ticket/controller/FilmTicketController.java
+++ src/main/java/com/cinema/ticket/controller/FilmTicketController.java
@@ -3,6 +3,7 @@ package com.cinema.ticket.controller;
 import com.cinema.ticket.model.FilmTicket;
 import com.cinema.ticket.service.FilmTicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ public class FilmTicketController {
     @Autowired
     private FilmTicketService filmTicketService;
 
+    @Value("${payment.type}")
+    private String paymentType;
+
     @GetMapping
     public List<FilmTicket> findFilms() {
         return filmTicketService.findFilms();
@@ -25,5 +29,10 @@ public class FilmTicketController {
     @GetMapping("/{title}")
     public List<FilmTicket> findFilmStartTimes(@PathVariable final String title) {
         return filmTicketService.findFilmStartTimes(title);
+    }
+
+    @GetMapping("/paymentType")
+    public String getPaymentType() {
+        return paymentType;
     }
 }
